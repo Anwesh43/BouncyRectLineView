6aa9027126a51b9aba893b88548ca25afdfb64d9
Anwesh43 <anweshthecool0@gmail.com>
2019-11-20 11:10:20 +0530

    adding canvas extension methods

--- bouncyrectlineview/src/main/java/com/anwesh/uiprojects/bouncyrectlineview/BouncyRectLineView.kt
+++ bouncyrectlineview/src/main/java/com/anwesh/uiprojects/bouncyrectlineview/BouncyRectLineView.kt
@@ -20,9 +20,31 @@ val sizeFactor : Float = 2.9f
 val delay : Long = 30
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rHFactor : Float = 3f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (this) * Math.PI / 2).toFloat()
+
+fun Canvas.drawBouncyRectLine(size : Float, scale : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sc : Float = scale.divideScale(1, 2).cosify()
+    drawRect(RectF(-size, -size / rHFactor, -size + 2 * size * sf, size / rHFactor), paint)
+    drawLine(size, 0f, size - 2 * size * sc, 0f, paint)
+}
+
+fun Canvas.drawBRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawBouncyRectLine(size, scale, paint)
+    restore()
+}
