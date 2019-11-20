fdf304503b32e8bcbb78410b6eaeb1013a355bcf
Anwesh43 <anweshthecool0@gmail.com>
2019-11-20 11:13:11 +0530

    adding BouncyRectLineView class

--- bouncyrectlineview/src/main/java/com/anwesh/uiprojects/bouncyrectlineview/BouncyRectLineView.kt
+++ bouncyrectlineview/src/main/java/com/anwesh/uiprojects/bouncyrectlineview/BouncyRectLineView.kt
@@ -48,3 +48,21 @@ fun Canvas.drawBRLNode(i : Int, scale : Float, paint : Paint) {
     drawBouncyRectLine(size, scale, paint)
     restore()
 }
+
+class BouncyRectLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
